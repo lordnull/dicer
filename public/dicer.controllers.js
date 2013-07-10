2e297668d6825b801fff600ba8ba04a765fc3bed
Micah Warren <micahw@lordnull.com>
2013-07-10 00:26:08 -0500

    Feature parity, and deletion of rolls/stats.

--- public/dicer.controllers.js
+++ public/dicer.controllers.js
@@ -13,7 +13,6 @@ Controllers.controller("DicerCtrl", function($scope){
 
 Controllers.controller("CharactersListCtrl", function($scope){
 //function CharactersListCtrl($scope) {
-	console.log('der character scope thing');
 	$scope.characters = dustbin.query('dicer.character');
 
 	$scope.createCharacter = function(){
@@ -25,90 +24,85 @@ Controllers.controller("CharactersListCtrl", function($scope){
 		$scope.characters.push(newCharacter);
 		$scope.newName = '';
 	}
-
-	$scope.loadCharacter = function(character){
-		console.log('load', character);
-	}
 });
 
-Controllers.controller("StatsListCtrl", function($scope){
-//function StatsListCtrl($scope) {
-	$scope.$watch('selectedCharacter', function(dahDude){
-			if(dahDude){
-				$scope.stats = dustbin.query('dicer.stat').filter(function(stat){
-					return stat.character_key == dahDude.$metadata.key;
-				});
-			} else {
-				$scope.stats = [];
-			}
+Controllers.controller("CharacterViewCtrl", function($scope, $routeParams){
+	var character = dustbin.get("dicer.character")[$routeParams.characterId];
+	$scope.character = character;
+
+	$scope.stats = dustbin.query("dicer.stat").filter(function(stat){
+		return stat.character_key == character.$metadata.key;
 	});
 
+	$scope.rolls = dustbin.query("dicer.roll").filter(function(roll){
+		return roll.character_key == character.$metadata.key;
+	});
+
+	$scope.newStatName = '';
 	$scope.createStat = function(){
-		if(! $scope.selectedCharacter){
-			console.log('quicky stat create abort');
+		if($scope.newStatName == ''){
 			return;
 		}
 		var newStat = {
 			'name':$scope.newStatName,
-			'character_key':$scope.selectedCharacter.$metadata.key,
-			'value':10
+			'character_key':$scope.character.$metadata.key,
+			'value': 10
 		};
-		var newStatKey = dustbin.store('dicer.stat', newStat);
-		newStat = dustbin.get('dicer.stat', newStatKey);
+		var newStatKey = dustbin.store("dicer.stat", newStat);
+		newStat = dustbin.get("dicer.stat", newStatKey);
 		$scope.stats.push(newStat);
 		$scope.newStatName = '';
-	}
+	};
 
-	$scope.updateStat = function(statObj){
-		var storable = dustbin.store(statObj);
-	}
-});
+	$scope.removeStat = function(stat){
+		$scope.stats = $scope.stats.filter(function(s){
+			return s != stat;
+		});
+		dustbin.remove("dicer.stat", stat.$metadata.key);
+	};
 
-Controllers.controller("RollsListCtrl", function($scope){
-//function RollsListCtrl($scope) {
-	$scope.$watch('selectedCharacter', function(dahDude){
-		if(dahDude){
-			$scope.rolls = dustbin.query('dicer.roll').filter(function(roll){
-				return roll.character_key == dahDude.$metadata.key;
-			});
-		} else {
-			$scope.rolls = [];
-		}
-	});
-
+	$scope.newRollName = '';
 	$scope.createRoll = function(){
-		if(! $scope.selectedCharacter){
+		if($scope.newRollName == ''){
 			return;
 		}
 		var newRoll = {
 			'name':$scope.newRollName,
-			'character_key': $scope.selectedCharacter.$metadata.key,
-			'string': '1d20'
+			'character_key':$scope.character.$metadata.key,
+			'value': '1d20'
 		};
-		var newRollKey = dustbin.store('dicer.roll', newRoll);
-		newRoll = dustbin.get('dicer.roll', newRollKey);
+		var newRollKey = dustbin.store("dicer.roll", newRoll);
+		newRoll = dustbin.get("dicer.roll", newRollKey);
 		$scope.rolls.push(newRoll);
 		$scope.newRollName = '';
-	}
+	};
 
-	$scope.storeRoll = function(rollObj){
-		console.log('der roll obj', arguments);
-		dustbin.store(rollObj);
-	}
-
-	$scope.rollDice = function(roll){
-		var statObjs = dustbin.query('dicer.stat').filter(function(st){
-			return st.character_key == $scope.selectedCharacter.$metadata.key;
+	$scope.removeRoll = function(roll){
+		$scope.rolls = $scope.rolls.filter(function(r){
+			return r != roll;
 		});
-		var rollScope = statObjs.reduce(function(acc, st){
+		dustbin.remove("dicer.roll", roll.$metadata.key);
+	};
+
+	$scope.maxHistory = 100;
+	$scope.rollHistory = [];
+	$scope.rollError = '';
+	$scope.rollDice = function(roll){
+		var rollScope = $scope.stats.reduce(function(acc, st){
 			acc[st.name] = st.value;
 			return acc;
 		}, {});
-		console.log('I should likely roll this:', roll.string, rollScope);
-		var parsed = dice.parse.parse(roll.string);
-		var evaled = dice.eval.eval(parsed, rollScope);
-		$scope.rollHistory.unshift(evaled);
-		console.log("I got", evaled);
-	}
+		try{
+			var parsed = dice.parse.parse(roll.string);
+			var evaled = dice.eval.eval(parsed, rollScope);
+			$scope.rollHistory.unshift(evaled);
+			if($scope.rollHistory.length > $scope.maxHistory){
+				$scope.rollHistory.pop();
+			}
+			$scope.rollError = '';
+		} catch(err){
+			$scope.rollError = err.toString();
+		}
+	};
 });
 
